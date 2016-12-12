9f3ac32897ff2139abe3e3c87e0905fdcfb4fdb0
Daniel Bahrdt <daniel.git@funroll-loops.de>
2016-12-12 17:17:32 +0100

    [common] BitCount::update add iterator version

--- common/stats.h
+++ common/stats.h
@@ -17,10 +17,24 @@ struct BitCount {
 	std::size_t sumNofBitsDenom;
 	BitCount();
 	void update(mpq_class v);
+	template<typename T_INPUT_ITERATOR>
+	void update(T_INPUT_ITERATOR begin, const T_INPUT_ITERATOR & end);
 	void print(std::ostream & out) const;
 };
 
 std::ostream & operator<<(std::ostream& out, const BitCount & bc);
+
+}//end namespace LIB_RATSS_NAMESPACE
+
+namespace LIB_RATSS_NAMESPACE {
+
+
+template<typename T_INPUT_ITERATOR>
+void BitCount::update(T_INPUT_ITERATOR begin, const T_INPUT_ITERATOR & end) {
+	for(; begin != end; ++begin) {
+		update(*begin);
+	}
+}
 
 }//end namespace LIB_RATSS_NAMESPACE
 
