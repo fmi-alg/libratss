87dd78f1f6eb933309fad1ef84e7a0d3ebfc6cca
Martin Seybold <seybold@fmi.uni-stuttgart.de>
2016-12-20 11:35:18 +0100

    added geoGrid generator to produce an equiAngle grid on 3D Sphere

--- tools/rndpoints.cpp
+++ tools/rndpoints.cpp
@@ -14,7 +14,7 @@
 using namespace LIB_RATSS_NAMESPACE;
 using namespace LIB_RATSS_NAMESPACE::tools;
 
-typedef enum {GT_NPLANE, GT_NSPHERE, GT_CGAL, GT_GEO } GeneratorType;
+typedef enum {GT_NPLANE, GT_NSPHERE, GT_CGAL, GT_GEO, GT_GEOGRID } GeneratorType;
 
 struct PointGenerator {
 	virtual OutputPoint generate(int dimension) = 0;
@@ -82,6 +82,51 @@ struct GeoPointGenerator: PointGenerator {
 	}
 };
 
+struct GeoGridGenerator: PointGenerator {
+	ProjectS2 proj;
+
+	virtual OutputPoint generate(int dimension) override {
+		return OutputPoint();
+	}
+	
+	virtual bool supports(int dimension) const override {
+		return (dimension == 3);
+	}
+        
+        std::vector<OutputPoint> generateAll( uint32_t nofSlices ){
+            // nofSlices >= 1== number of slices on an eighth sphere
+            std::vector<OutputPoint> result(0);
+            if( nofSlices < 1)
+                return result;
+            
+            OutputPoint northPole(3);
+            northPole.coords = { 0, 0, 1 };
+            result.push_back( northPole );     // north pole
+            
+            // lat in [-90,+90]
+            // lon in [0,360)
+            
+            const double angleInc = 90.0 / nofSlices;
+            
+            for( int sLat=1; sLat <2*nofSlices; ++sLat ){
+                double lat = +90.0 - angleInc * sLat;
+                
+                for( int sLon=0; sLon < 4*nofSlices; ++sLon ){
+                    OutputPoint ret(3);
+                    double lon = angleInc * sLon;
+                    proj.projectFromGeo( mpfr::mpreal(lat), mpfr::mpreal(lon), ret.coords[0], ret.coords[1], ret.coords[2]);
+                    result.push_back( ret );
+                }
+            }
+            
+            OutputPoint southPole(3);
+            southPole.coords = { 0, 0, -1 };
+            result.push_back( southPole );     // south pole
+            
+            return result;
+        }
+};
+
 struct NPlanePointGenerator: PointGenerator {
 	std::uniform_real_distribution<double> circleRnd;
 	std::uniform_int_distribution<uint32_t> boolRnd;
@@ -147,7 +192,7 @@ struct NSpherePointGenerator: PointGenerator {
 void help(std::ostream & out) {
 	out << "prg OPTIONS\n"
 		"Options:\n"
-		"-g generator\tgenerator = (nplane|nsphere|cgal|geo)\n"
+		"-g generator\tgenerator = (nplane|nsphere|cgal|geo|geoGrid)\n"
 		"-f format\tformat = (rational|split|float|float128)"
 		"-d dimensions\n"
 		"-n number\tnumber of points to create\n"
@@ -180,6 +225,9 @@ struct Config {
 					else if (gtStr == "geo") {
 						gt = GT_GEO;
 					}
+                                        else if (gtStr == "geoGrid"){
+                                                gt = GT_GEOGRID;
+                                        }
 					else {
 						std::cerr << "Unsupported generator: " << gtStr << std::endl;
 						return -1;
@@ -267,7 +315,22 @@ int main(int argc, char ** argv) {
 	else if (cfg.gt == GT_GEO) {
 		pg = new GeoPointGenerator();
 	}
+	else if (cfg.gt == GT_GEOGRID) {
+		GeoGridGenerator myPg;
+                if (!myPg.supports(cfg.dimension)) {
+                    std::cerr << "Selected generator does not support the selected dimension" << std::endl;
+                    return -1;
+                }
+                std::vector<OutputPoint> gridPoints = myPg.generateAll( cfg.count );
+                for( OutputPoint & p : gridPoints ){
+                    p.print(std::cout, cfg.ft);
+                    std::cout << '\n';
+                }
+                return 0;
+	}
+                
 	
+        
 	if (!pg->supports(cfg.dimension)) {
 		std::cerr << "Selected generator does not support the selected dimension" << std::endl;
 		delete pg;
