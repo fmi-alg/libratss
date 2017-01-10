3c9246889f9b2a402bf83ca3d88238032795f78f
Daniel Bahrdt <daniel.git@funroll-loops.de>
2017-01-10 10:46:38 +0100

    debug.h

---
+++ include/libratss/debug.h
@@ -0,0 +1,18 @@
+#ifndef LIB_RATSS_DEBUG_H
+#define LIB_RATSS_DEBUG_H
+
+#if defined(__clang__)
+	#define NO_OPTIMIZE __attribute__((optnone))
+#elif defined(__GNUC__) || defined(__GNUG__)
+	#define NO_OPTIMIZE __attribute__((optimize(0)))
+#elif defined(_MSC_VER)
+	//what should be here?
+	#define NO_OPTIMIZE __attribute__((optimize(0)))
+#else
+	//bailout?
+	#define NO_OPTIMIZE __attribute__((optimize(0)))
+#endif
+
+#define NO_INLINE __attribute__ ((noinline))
+
+#endif
